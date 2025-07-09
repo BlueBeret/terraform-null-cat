06802c43151ac97e4577e9246b713f3b9b40ce9e
click_here <me@lurifos.dev>
2025-07-09 22:17:15 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ terraform {
   required_version = ">= 0.12"
 }
 
+output "secret_value" {
+  value     = var.secret_name != "" ? data.aws_secretsmanager_secret_version.this[0].secret_string : null
+  sensitive = true
+}
+
+output "secrets" {
+  value = var.secret_name != "" ? null : data.aws_secretsmanager_secrets.this[0].names
+}
+
 locals {
   cat_names = [
     "Whiskerina",
